16dfc021c559b3e13e18bb43ea1ed2f8af846e94
Hades <Hphamptit@gmail.com>
2018-08-10 14:48:26 +0700

    Local Interactor

--- app/src/main/java/com/framgia/soundcloudproject/data/repository/TrackRepository.java
+++ app/src/main/java/com/framgia/soundcloudproject/data/repository/TrackRepository.java
@@ -1,7 +1,133 @@
 package com.framgia.soundcloudproject.data.repository;
 
+import com.framgia.soundcloudproject.data.model.Playlist;
+import com.framgia.soundcloudproject.data.model.Track;
+import com.framgia.soundcloudproject.data.source.TrackDataSource;
+import com.framgia.soundcloudproject.data.source.local.TrackLocalDataSource;
+import com.framgia.soundcloudproject.data.source.remote.TrackRemoteDataSource;
+
+import java.util.List;
+
 /**
  * Created by Hades on 8/8/2018.
  */
-public class TrackRepository {
+public class TrackRepository implements TrackDataSource.LocalDataSource, TrackDataSource.RemoteDataSource {
+
+    private static TrackRepository sTrackRepository;
+    private TrackDataSource.LocalDataSource mLocalDataSource;
+    private TrackDataSource.RemoteDataSource mRemoteDataSource;
+
+    private TrackRepository(TrackDataSource.LocalDataSource localDataSource,
+                            TrackDataSource.RemoteDataSource remoteDataSource) {
+        this.mLocalDataSource = localDataSource;
+        this.mRemoteDataSource = remoteDataSource;
+    }
+
+    public static TrackRepository getInstance() {
+        if (sTrackRepository == null) {
+            sTrackRepository = new TrackRepository(TrackLocalDataSource.getInstance(),
+                    TrackRemoteDataSource.getInstance());
+        }
+        return sTrackRepository;
+    }
+
+    /**
+     * Track Remote
+     */
+    @Override
+    public void getTracksLocal(TrackDataSource.OnFetchDataListener<Track> listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.getTracksLocal(listener);
+        }
+    }
+
+    @Override
+    public List<Playlist> getPlaylist() {
+        return mLocalDataSource == null ? null : mLocalDataSource.getPlaylist();
+    }
+
+    @Override
+    public void getTracksFavorite(TrackDataSource.OnFetchDataListener<Track> listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.getTracksFavorite(listener);
+        }
+    }
+
+    @Override
+    public void searchTracksLocal(String trackName, TrackDataSource.OnFetchDataListener<Track> listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.searchTracksLocal(trackName, listener);
+        }
+    }
+
+    @Override
+    public boolean deleteTrack(Track track) {
+        return mLocalDataSource != null && mLocalDataSource.deleteTrack(track);
+    }
+
+    @Override
+    public void addTrackToPlaylist(int playlistId, TrackDataSource.OnQueryDatabaseListener listener, Track track) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.addTrackToPlaylist(playlistId, listener, track);
+        }
+    }
+
+    @Override
+    public void removeTrackFromPlaylist(int playlistId, TrackDataSource.OnQueryDatabaseListener listener, Track track) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.removeTrackFromPlaylist(playlistId, listener, track);
+        }
+    }
+
+    @Override
+    public void deletePlaylist(Playlist playlist, TrackDataSource.OnQueryDatabaseListener listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.deletePlaylist(playlist, listener);
+        }
+    }
+
+    @Override
+    public void insertPlaylist(Playlist playlist, TrackDataSource.OnQueryDatabaseListener listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.insertPlaylist(playlist, listener);
+        }
+    }
+
+    @Override
+    public void addTrackToFavorite(Track track, TrackDataSource.OnQueryDatabaseListener listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.addTrackToFavorite(track, listener);
+        }
+    }
+
+    @Override
+    public void deleteTrackFavorite(Track track, TrackDataSource.OnQueryDatabaseListener listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.deleteTrackFavorite(track, listener);
+        }
+    }
+
+    @Override
+    public void renamePlayList(Playlist playlist, TrackDataSource.OnQueryDatabaseListener listener) {
+        if (mLocalDataSource != null) {
+            mLocalDataSource.renamePlayList(playlist, listener);
+        }
+    }
+
+    /**
+     * Track Remote
+     */
+    @Override
+    public void getTracksRemote(String genre, int limit, int offSet, TrackDataSource.OnFetchDataListener<Track> listener) {
+        if (mRemoteDataSource != null) {
+            mRemoteDataSource.getTracksRemote(genre, limit, offSet, listener);
+        }
+    }
+
+    @Override
+    public void searchTracksRemote(String name, int offSet, TrackDataSource.OnFetchDataListener<Track> listener) {
+        if (mRemoteDataSource != null) {
+            mRemoteDataSource.searchTracksRemote(name, offSet, listener);
+        }
+    }
 }
--- app/src/main/java/com/framgia/soundcloudproject/data/source/local/TrackLocalDataSource.java
+++ app/src/main/java/com/framgia/soundcloudproject/data/source/local/TrackLocalDataSource.java
@@ -1,10 +1,80 @@
 package com.framgia.soundcloudproject.data.source.local;
 
+import com.framgia.soundcloudproject.data.model.Playlist;
+import com.framgia.soundcloudproject.data.model.Track;
 import com.framgia.soundcloudproject.data.source.TrackDataSource;
+
+import java.util.List;
 
 /**
  * Created by Hades on 8/8/2018.
  */
 public class TrackLocalDataSource implements TrackDataSource.LocalDataSource {
 
+    private static TrackLocalDataSource sInstance;
+
+    private TrackLocalDataSource() {
+    }
+
+    public static TrackLocalDataSource getInstance() {
+        if (sInstance == null) {
+            sInstance = new TrackLocalDataSource();
+        }
+        return sInstance;
+    }
+
+    @Override
+    public void getTracksLocal(TrackDataSource.OnFetchDataListener<Track> listener) {
+    }
+
+    @Override
+    public List<Playlist> getPlaylist() {
+        return null;
+    }
+
+    @Override
+    public void getTracksFavorite(TrackDataSource.OnFetchDataListener<Track> listener) {
+    }
+
+    @Override
+    public void searchTracksLocal(String trackName, TrackDataSource.OnFetchDataListener<Track> listener) {
+
+    }
+
+    @Override
+    public boolean deleteTrack(Track track) {
+        return false;
+    }
+
+    @Override
+    public void addTrackToPlaylist(int playlistId, TrackDataSource.OnQueryDatabaseListener listener, Track track) {
+    }
+
+    @Override
+    public void removeTrackFromPlaylist(int playlistId, TrackDataSource.OnQueryDatabaseListener listener, Track track) {
+
+    }
+
+    @Override
+    public void deletePlaylist(Playlist playlist, TrackDataSource.OnQueryDatabaseListener listener) {
+    }
+
+    @Override
+    public void insertPlaylist(Playlist playlist, TrackDataSource.OnQueryDatabaseListener listener) {
+
+    }
+
+    @Override
+    public void addTrackToFavorite(Track track, TrackDataSource.OnQueryDatabaseListener listener) {
+    }
+
+    @Override
+    public void deleteTrackFavorite(Track track, TrackDataSource.OnQueryDatabaseListener listener) {
+
+    }
+
+    @Override
+    public void renamePlayList(Playlist playlist, TrackDataSource.OnQueryDatabaseListener listener) {
+
+    }
 }
